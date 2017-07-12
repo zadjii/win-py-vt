3ce42c2df031b2f901b2b18d892876b94c53e1c0
Mike Griese <zadjii@gmail.com>
2017-07-11 17:57:45 -0700

    Create enable.py
    This is the sample

---
+++ enable.py
@@ -0,0 +1,11 @@
+# Turns VT output support on
+def enable_vt_support():
+    if os.name == 'nt':
+        import ctypes
+        hOut = ctypes.windll.kernel32.GetStdHandle(-11)
+        out_modes = ctypes.c_uint32()
+        ENABLE_VT_PROCESSING = ctypes.c_uint32(0x0004)
+        # ctypes.addressof()
+        ctypes.windll.kernel32.GetConsoleMode(hOut, ctypes.byref(out_modes))
+        out_modes = ctypes.c_uint32(out_modes.value | 0x0004)
+        ctypes.windll.kernel32.SetConsoleMode(hOut, out_modes)
